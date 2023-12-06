fbdcc4ac5c4eb1d01f5b097cdf435cda87d5cfd3
Alexandra Dernova <alexder@gmx.net>
2023-12-06 15:25:32 +0100

    Add Calculator Class, add Methods add,subtraction, multiplication and division

---
+++ src/main/java/counter/Calculator.java
@@ -0,0 +1,16 @@
+package counter;
+
+public class Calculator {
+    public static int add(int a, int b){
+        return a + b;
+    }
+    public static int subtraction(int a, int b){
+        return a - b;
+    }
+    public static int multiplication(int a, int b){
+        return a * b;
+    }
+    public static double division(double a, double b){
+        return a/b;
+    }
+}
--- src/main/java/counter/Main.java
+++ src/main/java/counter/Main.java
@@ -1,10 +1,14 @@
 package counter;
 
+import static counter.Calculator.*;
+import counter.Counter;
+
 public class Main {
     public static void main(String[] args) {
         Counter counter1 = new Counter();
         Counter counter2 = new Counter();
         Counter.incrementTotalCounter(2);
+
         //oder
 
         counter1.incrementTotalCounter(4);
@@ -14,6 +18,11 @@ public class Main {
         counter2.incrementInstanceCounter(3);
         System.out.println(counter1);
         System.out.println(counter2);
+
+        System.out.println("Die Summe ist: " + add(3,23));
+        System.out.println("Die Subtraktion ist: " + subtraction(54,23));
+        System.out.println("Die Produkt ist: " + multiplication(3,23));
+        System.out.println("Die Division ist: " + division(3,23));
 
     }
 }
